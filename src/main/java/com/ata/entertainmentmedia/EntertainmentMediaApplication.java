eb22848305d7b3c1d53ab9681622f53f9a8b4830
atuysal7 <atuysal7@gmail.com>
2023-08-25 21:54:43 +0300

    Initial commit

---
+++ src/main/java/com/ata/entertainmentmedia/EntertainmentMediaApplication.java
@@ -0,0 +1,13 @@
+package com.ata.entertainmentmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntertainmentMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntertainmentMediaApplication.class, args);
+	}
+
+}
