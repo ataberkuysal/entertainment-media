0620329043c15d3e74c405697b2db617882fbcff
atuysal7 <atuysal7@gmail.com>
2023-09-16 22:23:09 +0300

    First Update Endpoint implemented and tested

--- src/main/java/com/ata/entertainmentmedia/web/controllers/SerieController.java
+++ src/main/java/com/ata/entertainmentmedia/web/controllers/SerieController.java
@@ -4,13 +4,18 @@ import com.ata.entertainmentmedia.data.dtos.SerieDTO;
 import com.ata.entertainmentmedia.data.entities.Serie;
 import com.ata.entertainmentmedia.web.services.SerieService;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -22,6 +27,9 @@ public class SerieController {
     SerieService serieService;
     @Autowired
     ModelMapper modelMapper;
+
+    @Autowired
+    ModelMapper modelMapperPartial;
 
     //GET
 
@@ -53,17 +61,34 @@ public class SerieController {
         return new ResponseEntity<SerieDTO>(serieResponse, HttpStatus.CREATED);
     }
 
-//    @PutMapping("/update/{id}")
-//    Address replaceEmployee(@RequestBody Address newAddress, @PathVariable Long id) {
-//
-//        return repository.findById(id)
-//                .map(address -> {
-//                    address.setCity(newAddress.getCity());
-//                    address.setPin(newAddress.getPostalCode());
-//                    return repository.save(address);
-//                })
-//                .orElseGet(() -> {
-//                    return repository.save(newAddress);
-//                });
-//    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<SerieDTO> updateSerie(@RequestBody SerieDTO serieDTO, @PathVariable Long id) {
+
+        Serie serieToBeUpdated  = serieService.getSerieById(id);
+
+/*        List<Field> fields = Arrays.stream(serieToBeUpdated.getClass().getDeclaredFields())
+                .filter(Objects::nonNull).toList()
+                .stream().map(f -> f.getClass().getMethod("get{}",f.getName())).invoke(f);*/
+
+        modelMapperPartial.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+
+        serieToBeUpdated = modelMapperPartial.map(serieDTO, Serie.class);
+
+        serieService.saveSerie(serieToBeUpdated);
+
+        SerieDTO updatedSerieDTO = modelMapper.map(serieToBeUpdated, SerieDTO.class);
+
+        return ResponseEntity.ok().body(updatedSerieDTO);
+
+        /*return serieService.getSerieById(id)
+                .map(address -> {
+                    address.setCity(newAddress.getCity());
+                    address.setPin(newAddress.getPostalCode());
+                    return repository.save(address);
+                })
+                .orElseGet(() -> {
+                    return repository.save(newAddress);
+                });*/
+    }
+
 }
